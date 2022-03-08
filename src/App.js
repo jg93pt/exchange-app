0adbc84873ddc2451f3876f9a3f5318c236031ec
jg93pt <jorgebenf@gmail.com>
2022-03-08 14:13:31 +0000

    Test auto deploy heroku with github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">  
         <p>
-        <code>Hello World!</code>
+        Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
