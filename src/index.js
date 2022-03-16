128db56e2c16f17ac739a785b719e145b786da54
jg93pt <jorgebenf@gmail.com>
2022-03-16 21:54:54 +0000

    Creating Footer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './NavBar.css'
-import './CurrencyConverter.css'
-import './ExchangeRate.css'
+import './NavBar.css';
+import './CurrencyConverter.css';
+import './ExchangeRate.css';
+import './Footer.css';
 
 import './index.css';
 import App from './App';
